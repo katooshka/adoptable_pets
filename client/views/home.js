66ca453cae5cfd77d64cd2bdf3bce8f7acecba2a
katooshka <ek.zabara@gmail.com>
2018-03-25 11:48:13 +0100

    Add basic components structure

--- client/views/home.js
+++ client/views/home.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import { Header } from './Header.js';
+import { Main } from './Main.js';
+import { SearchOptions } from './SearchOptions.js';
+import { SearchResults } from './SearchResults.js';
+
+
 export default class Home extends React.Component {
 
   constructor() {
     super();
-    this.state = { name: "Kitty" };
+    this.state = { 
+      name: "Kitty",
+     };
     this.clickHandler = this.clickHandler.bind(this);
   }
 
@@ -14,9 +22,21 @@ export default class Home extends React.Component {
 
   render() {
     return (
-      <h1 onClick={this.clickHandler}>
-        {`Hello ${this.state.name}!`}
-      </h1>
+      <div>
+        <Header/>
+        <div>
+          <Main/>
+          <h1 onClick={this.clickHandler}>
+            {`Hello ${this.state.name}!`}
+          </h1>
+          <div>
+            <SearchOptions/>
+          </div>
+          <div>
+            <SearchResults/>
+          </div>
+        </div>
+      </div>
     );
   }
 }
