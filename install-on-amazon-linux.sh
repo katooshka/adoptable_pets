2baae53a478a89e603b0f2598f9ac3fc11840778
katooshka <ek.zabara@gmail.com>
2018-04-02 13:51:43 +0100

    Add installation script for Amazon Linux

---
+++ install-on-amazon-linux.sh
@@ -0,0 +1,44 @@
+#/bin/bash
+
+set -e
+
+echo 'Start script.'
+
+echo 'Installing git...'
+sudo yum install git-all -y
+
+echo 'Cloning repository...'
+git clone https://github.com/katooshka/adoptable_pets.git
+
+echo 'Navigate to the main app directory'
+cd adoptable_pets
+
+echo 'Installing NodeJS...'
+curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+sudo yum -y install nodejs
+
+echo 'Installing packages...'
+npm install
+
+echo 'Downloading MongoDB...'
+sudo touch /etc/yum.repos.d/mongodb-org-3.4.repo
+sudo chmod 777 /etc/yum.repos.d/mongodb-org-3.4.repo
+sudo cat <<EOF >/etc/yum.repos.d/mongodb-org-3.4.repo
+[mongodb-org-3.4]
+name=MongoDB 3.4 Repository
+baseurl=https://repo.mongodb.org/yum/amazon/2013.03/mongodb-org/3.4/x86_64/
+gpgcheck=0
+enabled=1
+EOF
+sudo yum install -y mongodb-org
+
+echo 'Starting MongoDB...'
+sudo service mongod start
+
+echo 'Importing data to MongoDB...'
+sudo npm install -g csvtojson
+./data/import.sh
+
+echo 'Starting the application...'
+sudo npm run build-prod
+
