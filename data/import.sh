92be999cd7ad078a2d9e462ba953dd1c3d09cf50
katooshka <ek.zabara@gmail.com>
2018-04-02 13:32:54 +0100

    Fix import script

--- data/import.sh
+++ data/import.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-csvtojson pets.csv > ./data/pets.json
+csvtojson ./data/pets.csv > ./data/pets.json
 echo 'CSV file parsed'
 
 mongo pets --eval "db.dropDatabase()"
 echo 'Pets collection cleared'
 
-mongoimport --db pets --collection pets --file pets.json --jsonArray
+mongoimport --db pets --collection pets --file ./data/pets.json --jsonArray
 echo 'Imported to Mongo'
